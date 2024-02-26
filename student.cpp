553c8cb373de8b4e6b52908ffc9b5f9d2be8a24f
G.Aarthi <119107887+AKKIAARTHI@users.noreply.github.com>
2024-02-26 06:46:14 +0530

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -1,58 +1,36 @@
-/*
-Using a constructor and destructor in C++, Write a C++ class 'Student' and assign the following :
-* string fullName
-* int rollNum
-* double semPerentage
-* string collegeName
-* int collegeCode
-*/
-
-#include <iostream>
+#include<iostream>
 using namespace std;
-
 class Student
 {
 public:
-    string fullName, collegeName;
-    int rollNumber, collegeCode;
-    double semPercentage;
-    Student(string sfullName, int srollNumber, double ssemPercentage, string scollegeName, int scollegeCode)
-    {
-        fullName = sfullName;
-        rollNumber = srollNumber;
-        semPercentage = ssemPercentage;
-        collegeName = scollegeName;
-        collegeCode = scollegeCode;
-    }
-    Student()
-    {
-        fullName = "Rahul S";
-        rollNumber = 572;
-        semPercentage = 90.0;
-        collegeName = "MVGRCE";
-        collegeCode = 33;
-    }
-    ~Student()
-    {
-        cout << "DEAD" << endl;
-    }
-
-    void display()
-    {
-        cout << "Full Name : " << fullName << endl;
-        cout << "Roll Number : " << rollNumber << endl;
-        cout << "Sem Percentage : " << semPercentage << endl;
-        cout << "College Name : " << collegeName << endl;
-        cout << "College Code : " << collegeCode << endl;
-    }
+string fullName;
+int rollNum;
+double semPerentage;
+string collegeName;
+int collegeCode;
+Student()
+{
+cout<<"Student class initialized!!\nIam a student\n";
+}
+~Student(){}
 };
-
 int main()
 {
-    Student student1;
-    Student student2("Shiva k", 589, 95, "MVGRCE", 33);
-    cout << "-------------------Student-1 Details----------------------" << endl;
-    student1.display();
-    cout << "-------------------Student-2 Details----------------------" << endl;
-    student2.display();
+Student winni;
+cout<<"Enter your name : ";
+getline(cin,winni.fullName);
+cout<<"Enter your roll number : ";
+cin>>winni.rollNum;
+cout<<"Enter your sem percentage : ";
+cin>>winni.semPerentage;
+cout<<"Enter your college name : ";
+cin>>winni.collegeName;
+cout<<"Enter your college code : ";
+cin>>winni.collegeCode;
+cout<<"Name : "<<winni.fullName<<endl;
+cout<<"RollNum : "<<winni.rollNum<<endl;
+cout<<"SemPercentage : "<<winni.semPerentage<<endl;
+cout<<"CollegeName : "<<winni.collegeName<<endl;
+cout<<"CollegeCode : "<<winni.collegeCode<<endl;
+return 0;
 }
